935bcd9a9bef00503d141ea893600fee8d8ca614
alfred_dev21 <mr.khanyile21@gmail.com>
2024-08-11 20:40:17 +0200

    Added more testCases

--- CalculatorConsoleApplicationTests.nUnitTests/CalculatorTests.cs
+++ CalculatorConsoleApplicationTests.nUnitTests/CalculatorTests.cs
@@ -31,5 +31,38 @@ namespace CalculatorConsoleApplicationTests.nUnitTests
 
             Assert.That(actual, Is.EqualTo(expected));
         }
+
+        [TestCase("sqrt(81)")]
+        public void Sqrt_Test1(string intruction)
+        {
+            calculator.SetInstruction(intruction);
+
+            string expected = "9";
+            string actual = calculator.CalculatorHandler();
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase("-10 / 2")]
+        public void Division_Test1(string intruction)
+        {
+            calculator.SetInstruction(intruction);
+
+            string expected = "-5";
+            string actual = calculator.CalculatorHandler();
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase("-2 * 7")]
+        public void multiplication_Test1(string intruction)
+        {
+            calculator.SetInstruction(intruction);
+
+            string expected = "-14";
+            string actual = calculator.CalculatorHandler();
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
     }
 }
